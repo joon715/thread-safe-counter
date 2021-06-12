d7b318464f65ce6e7349b80d079d83b532be26ae
joon715 <kimwj6683@naver.com>
2021-06-12 12:15:10 +0900

    Update 2

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ Developed in WSL2 environment.<br/>
 
 "Upgrade list"<br/>
 
-##1. Create semaphore.c from copy of tscounter.c
--> Understand the structure of tscounter.c
--> Be ready to make semaphore.c file using semaphore.
+##1. Create semaphore.c from copy of tscounter.c<br/>
+-> Understand the structure of tscounter.c<br/>
+-> Be ready to make semaphore.c file using semaphore.<br/>
+
+##2. Edit semaphore.c to lock the thread with semaphore<br/>
+-> Alternate Mutex part with Semaphore.<br/>
+-> Use IPC_PRIVATE to create key.<br/>
+-> Create sema_lock, sema_unlock function to lock the threads more simple.<br/>
+
--- semaphore.c
+++ semaphore.c
@@ -1,67 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+int semid;
 
 typedef struct __counter_t {
-    int value;
-    pthread_mutex_t lock;
+	int value;
 } counter_t;
+
+union semun{
+	int val;
+	struct semid_ds *buf;
+	unsigned short *array;
+};
 
 unsigned int loop_cnt;
 counter_t counter;
 
+void sema_lock(){
+	struct sembuf s;
+	s.sem_num = 0;
+	s.sem_op = -1;
+	s.sem_flg = 0;
+	semop(semid,&s,1);
+}
+
+void sema_unlock(){
+	struct sembuf s;
+	s.sem_num = 0;
+	s.sem_op = 1;
+	s.sem_flg = 0;
+	semop(semid,&s,1);
+}
+
 void init(counter_t *c) {
-    c->value = 0;
-    pthread_mutex_init(&c->lock, NULL);
+	c->value = 0;
 }
 
 void increment(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
-    c->value++;
-    pthread_mutex_unlock(&c->lock);
+	sema_lock();
+	c->value++;
+	sema_unlock();
 }
 
 void decrement(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
-    c->value--;
-    pthread_mutex_unlock(&c->lock);
+	sema_lock();
+	c->value--;
+	sema_unlock();
 }
 
 int get(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
-    int rc = c->value;
-    pthread_mutex_unlock(&c->lock);
-    return rc;
+	sema_lock();
+	int rc = c->value;
+	sema_unlock();
+	return rc;
 }
 
 void *mythread(void *arg)
 {
-    char *letter = arg;
-    int i;
+	char *letter = arg;
+	int i;
 
-    printf("%s: begin\n", letter);
-    for (i = 0; i < loop_cnt; i++) {
-        increment(&counter);
-    }
-    printf("%s: done\n", letter);
-    return NULL;
+	printf("%s: begin\n", letter);
+	for (i = 0; i < loop_cnt; i++) {
+	increment(&counter);
+	}
+	printf("%s: done\n", letter);
+	return NULL;
 }
+
                                                                              
 int main(int argc, char *argv[])
 {                    
-    loop_cnt = atoi(argv[1]);
+	loop_cnt = atoi(argv[1]);
+	union semun su;
 
-    init(&counter);
+	// using setget to get semid
+	// using IPC_PRIVATE to create key
+	if((semid = semget(IPC_PRIVATE, 1, IPC_CREAT|0660)) == -1){
+		printf("semet() fail \n");
+		exit(0);
+        }
 
-    pthread_t p1, p2;
-    printf("main: begin [counter = %d]\n", get(&counter));
-    pthread_create(&p1, NULL, mythread, "A"); 
-    pthread_create(&p2, NULL, mythread, "B");
-    // join waits for the threads to finish
-    pthread_join(p1, NULL); 
-    pthread_join(p2, NULL); 
-    printf("main: done [counter: %d] [should be: %d]\n", get(&counter), loop_cnt * 2);
-    return 0;
+        su.val=1;
+        if(semctl(semid,0,SETVAL, su) == -1){
+                printf("semctl fail\n");
+                exit(0);
+        }
+
+
+	init(&counter);
+
+	pthread_t p1, p2;
+	printf("main: begin [counter = %d]\n", get(&counter));
+	pthread_create(&p1, NULL, mythread, "A"); 
+	pthread_create(&p2, NULL, mythread, "B");
+	// join waits for the threads to finish
+	pthread_join(p1, NULL); 
+	pthread_join(p2, NULL); 
+	printf("main: done [counter: %d] [should be: %d]\n", get(&counter), loop_cnt * 2);
+
+	// destroy the semid using semctl
+	if(semctl(semid, 0, IPC_RMID, su) == -1){
+		 printf("semctl() fail\n");
+		 exit(0);
+	}
+
+	return 0;
 }
